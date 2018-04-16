282deebc4eb2eec0faa59c5f91062d085d97481e
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-16 19:45:38 +0100

    assertion test passed

--- Part_B/specs/sports_team_spec.rb
+++ Part_B/specs/sports_team_spec.rb
@@ -5,26 +5,36 @@ require_relative("../sports_team.rb")
 
 class TestSportsTeam < MiniTest::Test
 
-  def test_get_team_name
+  def test_team_name
     team = SportsTeam.new("Warriors",["Anna","Sophie","Sarah"],"Kate")
-    assert_equal("Warriors", team.get_team_name())
+    assert_equal("Warriors", team.team_name())
   end
 
-  def test_get_players
+  def test_players
     team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
-    assert_equal(["Anna", "Sophie", "Sarah"], team.get_players())
+    assert_equal(["Anna", "Sophie", "Sarah"], team.players())
   end
 
-  def test_get_coach
+  # def test_get_coach
+  #   team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
+  #   assert_equal("Kate", team.get_coach())
+  # end
+
+  # def test_set_coach
+  #   team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
+  #   team.set_coach("Tina")
+  #   assert_equal("Tina", team.get_coach())
+  # end
+
+  def test_coach
     team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
-    assert_equal("Kate", team.get_coach())
+    team.coach = ("Tina")
+    assert_equal("Tina", team.coach())
   end
 
-  def test_set_coach
-    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
-    team.set_coach("Tina")
-    assert_equal("Tina", team.get_coach())
+  def test_add_new_players
+  #   team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
+  #   assert_equal(["Anna", "Sophie", "Sarah"], team.players())
   end
-
 
 end
--- Part_B/sports_team.rb
+++ Part_B/sports_team.rb
@@ -1,7 +1,7 @@
 class SportsTeam
 
-  # attr_reader(:team_name, :players)
-  # attr_accessor(:coach)
+  attr_reader(:team_name, :players)
+  attr_accessor(:coach)
 
   def initialize (input_team_name, input_players, input_coach)
     @team_name = input_team_name
@@ -10,21 +10,21 @@ class SportsTeam
 
   end
 
-  def get_team_name()
-    return @team_name
-  end
+  # def get_team_name()
+  #   return @team_name
+  # end
+  #
+  # def get_players()
+  #   return @players
+  # end
 
-  def get_players()
-    return @players
-  end
-
-  def get_coach()
-    return @coach
-  end
-
-  def set_coach(new_name)
-      @coach = new_name
-  end
+  # def get_coach()
+  #   return @coach
+  # end
+  #
+  # def set_coach(new_name)
+  #     @coach = new_name
+  # end
 
 
 end
