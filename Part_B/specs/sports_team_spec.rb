64beb5139e45c8f4d21ad9fab3d3f5c2355b9fa0
sarahmurphy86 <sarah.graham86@gmail.com>
2018-04-17 09:01:47 +0100

    Homework completed

--- Part_B/specs/sports_team_spec.rb
+++ Part_B/specs/sports_team_spec.rb
@@ -6,12 +6,12 @@ require_relative("../sports_team.rb")
 class TestSportsTeam < MiniTest::Test
 
   def test_team_name
-    team = SportsTeam.new("Warriors",["Anna","Sophie","Sarah"],"Kate")
+    team = SportsTeam.new("Warriors",["Anna","Sophie","Sarah"],"Kate", 0)
     assert_equal("Warriors", team.team_name())
   end
 
   def test_players
-    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate", 0)
     assert_equal(["Anna", "Sophie", "Sarah"], team.players())
   end
 
@@ -27,14 +27,43 @@ class TestSportsTeam < MiniTest::Test
   # end
 
   def test_coach
-    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate", 0)
     team.coach = ("Tina")
     assert_equal("Tina", team.coach())
   end
 
-  def test_add_new_players
-  #   team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
-  #   assert_equal(["Anna", "Sophie", "Sarah"], team.players())
+  def test_add_new_player
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate", 0)
+    team.add_new_player_name("John")
+    assert_equal(["Anna", "Sophie", "Sarah", "John"], team.players())
   end
+
+# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+#
+  def test_find_player_by_name
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate", 0)
+    team.find_player_by_name("Anna")
+    assert_equal("Anna",team.find_player_by_name("Anna"))
+  end
+#
+
+  def test_add_points
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate", 0)
+  end
+# # create a method that takes in whether the team has won or lost and updates the points property for a win.
+#
+  def test_team_outcome__win
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate", 0)
+    team.team_outcome("WIN")
+    assert_equal(3, team.team_outcome("WIN"))
+  end
+
+
+  def team_outcome__loose
+    team = SportsTeam.new("Warriors", ["Anna", "Sophie", "Sarah"], "Kate")
+    team.team_outcome("LOOSE")
+    assert_equal(-3, team.team_outcome("LOOSE"))
+  end
+
 
 end
--- Part_B/sports_team.rb
+++ Part_B/sports_team.rb
@@ -3,12 +3,33 @@ class SportsTeam
   attr_reader(:team_name, :players)
   attr_accessor(:coach)
 
-  def initialize (input_team_name, input_players, input_coach)
-    @team_name = input_team_name
-    @players = input_players
-    @coach = input_coach
+    def initialize (input_team_name, input_players, input_coach, points_total)
+      @team_name = input_team_name
+      @players = input_players
+      @coach = input_coach
+      @points = points_total
 
-  end
+    end
+
+    def add_new_player_name(extra_name)
+      @players.push(extra_name)
+    end
+
+    def find_player_by_name(name_to_find)
+      player = []
+      for player in @players
+        return player if player == (name_to_find)
+      end
+    end
+    #
+    def team_outcome(win_or_loose)
+      @points = 0
+        if win_or_loose == "WIN"
+          @points +=3
+        else
+          @points -=3
+        end
+    end
 
   # def get_team_name()
   #   return @team_name
